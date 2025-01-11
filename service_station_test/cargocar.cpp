8c7c58b8a7906cf0bf7cadb29c2f324f7b492a9f
Ilya <i.mazok@yahoo.com>
2025-01-11 11:09:52 +0300

    removed trash

--- service_station_test/cargocar.cpp
+++
@@ -1,4 +0,0 @@
-#include "cargocar.h"
-
-CargoCar::CargoCar( std::string name ) : Car( name )
-{}
--- service_station_test/passengercar.cpp
+++
@@ -1,4 +0,0 @@
-#include "passengercar.h"
-
-PassengerCar::PassengerCar( std::string name ) : Car( name )
-{}
