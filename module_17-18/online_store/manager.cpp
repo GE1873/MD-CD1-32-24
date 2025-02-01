e6651ee0e4cf64c809b0635ba1ce159b9c2f497b
Ilya <i.mazok@yahoo.com>
2025-02-01 18:14:01 +0300

    added classes

---
+++ module_17-18/online_store/manager.cpp
@@ -0,0 +1,78 @@
+#include "manager.h"
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+//#include <chrono>
+//#include <ctime>
+
+Manager::Manager()
+{
+    _orders.reserve(100);
+}
+
+Manager::~Manager()
+{
+    _orders.clear();
+    _processingOrders.clear();
+}
+
+void Manager::addOrder( double orderAmount )
+{
+    if( _orders.size() >= 100 ) {
+        std::cout << "Too many orders, please wait." << std::endl;
+    } else _orders.emplace_back( orderAmount );
+}
+
+void Manager::processOrders()
+{
+    std::transform( make_move_iterator( _orders.begin() ), make_move_iterator( _orders.end() ), _orders.begin(),
+                   []( Order order ) {
+                       time_t now = time( 0 );
+                       tm* ltm = localtime( &now );
+                       std::time_t th1 = std::mktime( ltm );
+                       std::time_t th2 = order.getOrderDate();
+                       if( std::difftime( th1, th2 ) > 5 ) order.updateStatus( Order::OrderStatus::Processing );
+                       return order;
+                   });
+
+    auto oi = std::find_if( _orders.begin(), _orders.end(),
+                           []( const Order& o ) { return o.getOrderAmount() > 5000 && o.getOrderStatus() == Order::OrderStatus::Processing;} );
+
+    std::cout << std::left << std::setw(30) << "First order number over 5000 is " <<  oi->getOrderNumber() << " in amount " <<  oi->getOrderAmount() << std::endl;
+
+    auto poi = std::partition( _orders.begin(), _orders.end(), []( const Order& o )
+                              {
+                                  return o.getOrderStatus() == Order::OrderStatus::Processing;
+                              });
+
+    _processingOrders.insert( _processingOrders.end(),
+                             std::make_move_iterator( _orders.begin() ),
+                             std::make_move_iterator( poi ) );
+
+    _orders.erase(_orders.begin(), poi);
+}
+
+void Manager::printIncomingOrders() const
+{
+    printOrders( _orders );
+}
+
+void Manager::printProcessingOrders() const
+{
+    printOrders( _processingOrders );
+}
+
+void Manager::printOrders( const std::vector<Order>& orders ) const
+{
+    for( const auto& order : orders ) {
+        std::cout << "------------------------------------------------------" << std::endl;
+        std::cout << std::left << std::setw( 30 ) << "Order number : " <<  order.getOrderNumber() << std::endl;
+        std::cout << std::left << std::setw( 30 ) << "Order status : " <<  order.getOrderStatusName( order.getOrderStatus() ) << std::endl;
+        std::cout << std::left << std::setw( 30 ) << "Order amount : " <<  order.getOrderAmount() << std::endl;
+        char dateString[100];
+        std::time_t t = order.getOrderDate();
+        std::tm* olt = localtime( &t );
+        strftime( dateString, sizeof( char )*100, "%B %d, %Y, %H:%M:%S", olt );
+        std::cout << std::left << std::setw( 30 ) << "Order date : " << dateString << std::endl;
+    }
+}
---
+++ module_17-18/online_store/order.cpp
@@ -0,0 +1,93 @@
+#include "order.h"
+#include <iostream>
+
+Order::Order( double orderAmount ) : _orderAmount{ orderAmount }
+{
+    _orderNumber = ++_counter;
+    std::cout << "The order number " << _orderNumber <<" created! (constructor)" << std::endl;
+}
+
+Order::Order( Order&& order )
+{
+    this->_orderAmount = order._orderAmount;
+    this->_orderDate = order._orderDate;
+    this->_orderStatus = order._orderStatus;
+    this->_orderNumber = order._orderNumber;
+    std::cout << "The order moved! (move constructor)" << std::endl;
+}
+
+Order::Order( Order& order )
+{
+    _orderAmount = order.getOrderAmount();
+    _orderDate = createOrderDate();
+    _orderStatus = OrderStatus::New;
+    _orderNumber = ++_counter;
+    std::cout << "The order copyed! (copy constructor)" << std::endl;
+}
+
+std::time_t Order::createOrderDate() const
+{
+    time_t now = time( 0 );
+    tm* ltm = localtime( &now );
+    return std::mktime( ltm );
+}
+
+int Order::getOrderNumber() const
+{
+    return _orderNumber;
+}
+
+Order& Order::operator=( Order&& order )
+{
+    if ( this != &order ) {
+        this->_orderAmount = order._orderAmount;
+        this->_orderDate = order._orderDate;
+        this->_orderStatus = order._orderStatus;
+        this->_orderNumber = order._orderNumber;
+        std::cout << "The order moved! (move assignment operator)" << std::endl;
+    }
+    return *this;
+}
+
+Order::OrderStatus Order::getOrderStatus() const
+{
+    return _orderStatus;
+}
+
+std::time_t Order::getOrderDate() const
+{
+    return _orderDate;
+}
+
+std::string Order::getOrderStatusName( OrderStatus status ) const
+{
+    std::string res{};
+    switch ( status ) {
+    case OrderStatus::New:
+        res = "New";
+        break;
+    case OrderStatus::Processing:
+        res = "Processing";
+        break;
+    case OrderStatus::Shipped:
+        res = "Shipped";
+        break;
+    case OrderStatus::Unknown:
+        res = "Unknown";
+        break;
+    default:
+        res = "Unknown";
+        break;
+    }
+    return res;
+}
+
+void Order::updateStatus( OrderStatus status )
+{
+    _orderStatus = status;
+}
+
+double Order::getOrderAmount() const
+{
+    return _orderAmount;
+}
